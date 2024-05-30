3f9f3d2f6a3238124c3a2d818a136f3b04579c5f
JThoe26 <jackson.thoe@mnsu.edu>
2024-05-29 23:47:45 -0500

    base app created

---
+++ src/App/App.jsx
@@ -0,0 +1,10 @@
+import './App.less';
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const HotModuleReplacementPluginConfig = new webpack.HotModuleReplacementPlugin();
+
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './src/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
+const CleanWebpackPluginConfig = new CleanWebpackPlugin();
+
+module.exports = {
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  mode: 'development',
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.(less|css)$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+      test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      }
+    ]
+  },
+  plugins: [CleanWebpackPluginConfig, 
+            HtmlWebpackPluginConfig, 
+            new MiniCssExtractPlugin({
+              filename: 'styles.css',
+            }), 
+            HotModuleReplacementPluginConfig],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    publicPath: '/'
+  }
+};

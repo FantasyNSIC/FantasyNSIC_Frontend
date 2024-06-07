f0b15082f5be7e534059daacbde17b4bba33e0f7
JThoe26 <jackson.thoe@mnsu.edu>
2024-06-07 14:54:56 -0500

    error message box

--- src/components/PlayersPage/PlayersPage.jsx
+++ src/components/PlayersPage/PlayersPage.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
 import { getAvailablePlayers } from "../../service/fantasyService.js";
 import { getLogoFunction } from "../../images/getLogoFuncion.js";
+import { FiAlertTriangle } from "react-icons/fi";
 import PageHeading from "../PageHeading/PageHeading.jsx";
 import PageSelectionBar from "../PageSelectionBar/PageSelectionBar.jsx";
 import "./PlayersPage.less";
@@ -15,42 +16,65 @@ const PlayersPage = () => {
     // state to hold the players
     const [resPlayers, setPlayers] = useState([]);
 
+    // state to hold active filter
+    const [activeFilter, setActiveFilter] = useState("ALL");
+
+    // state to hold search bar input
+    const [searchInput, setSearchInput] = useState("");
+
+    // state for displaying error message
+    const [showError, setShowError] = useState(false);
+    const [error, setError] = useState("");
+
     // fetch the players when the component renders
     useEffect(() => {
         const fetchPlayers = async () => {
-            const response = await getAvailablePlayers(league_id);
-            setPlayers(response);
+            try {
+                const response = await getAvailablePlayers(league_id);
+                setPlayers(response);
+            } catch (exception) {
+                setShowError(true);
+                setError(exception.message); // Access the message property of the error
+            }
         };
         fetchPlayers();
     }, []);
+
+    // handle filter change
+    function handleFilterChange(filter) {
+        setActiveFilter(filter);
+    }
 
     const PlayerObject = ({player}) => {
         const name = `${player.first_name} ${player.last_name}`
         return (
             <div className="players-page-player-object">
                 <img className="players-page-object-team-logo" src={getLogoFunction(player.team_id)} />
-                <div className="players-page-player-info"
-                style={{'--margin-left': `${15}%`}}>{name}</div>
-                <div className="players-page-player-info"
-                style={{'--margin-left': `${15}%`}}>{player.pos}</div>
-                <div className="players-page-player-info"
-                style={{'--margin-left': `${8}%`}}>{player.cls}</div>
-                <div className="players-page-player-info"
-                style={{'--margin-left': `${8}%`}}>{player.height}</div>
-                <div className="players-page-player-info"
-                style={{'--margin-left': `${8}%`}}>{player.weight}</div>
+                <div className="players-page-player-info">{name}</div>
+                <div className="players-page-player-info">{player.pos}</div>
+                <div className="players-page-player-info">{player.cls}</div>
+                <div className="players-page-player-info">{player.height}</div>
+                <div className="players-page-player-info">{player.weight}</div>
                 {/* TODO: implement player season fantasy points */}
-                <div className="players-page-player-info"
-                style={{'--margin-left': `${8}%`}}>{0}</div>
+                <div className="players-page-player-info">{0}</div>
             </div>
         )
     }
 
-    const PlayerObjectList = ({players}) => {
+    const PlayerObjectList = ({players, filter}) => {
         if (!players || players.length === 0) return (<div></div>);
+        if (filter !== "ALL") {
+            players = players.players.filter((player) => player.pos === filter);}
+        else {players = players.players;}
+
+        // filter by search input name
+        const nameFilteredPlayers = players.filter((player) => {
+            const name = `${player.first_name} ${player.last_name}`;
+            return name.toLowerCase().includes(searchInput.toLowerCase());
+        });
         return (
             <div className="players-page-player-object-list">
-                {players.players.map((player) => {
+                {nameFilteredPlayers.map((player) => {
                     return <PlayerObject key={player.player_id} player={player} />
                 })}
             </div>
@@ -67,13 +91,23 @@ const PlayersPage = () => {
                     <div className="players-page-filter-container">
                         <div className="players-page-available-players-text">Available Players</div>
                         <div className="players-page-filter-selection">
-                            <div className="players-page-filter-button">ALL</div>
-                            <div className="players-page-filter-button">QB</div>
-                            <div className="players-page-filter-button">RB</div>
-                            <div className="players-page-filter-button">WR</div>
-                            <div className="players-page-filter-button">TE</div>
-                            <div className="players-page-filter-button">K</div>
-                            <input className="players-page-search-bar" type="text" placeholder="Search" />
+                            <div className={`players-page-filter-button${activeFilter === "ALL" ? "-active" : ""}`}
+                                onClick={() => handleFilterChange("ALL")}>ALL</div>
+                            <div className={`players-page-filter-button${activeFilter === "QB" ? "-active" : ""}`}
+                                onClick={() => handleFilterChange("QB")}>QB</div>
+                            <div className={`players-page-filter-button${activeFilter === "RB" ? "-active" : ""}`}
+                                onClick={() => handleFilterChange("RB")}>RB</div>
+                            <div className={`players-page-filter-button${activeFilter === "WR" ? "-active" : ""}`}
+                                onClick={() => handleFilterChange("WR")}>WR</div>
+                            <div className={`players-page-filter-button${activeFilter === "TE" ? "-active" : ""}`}
+                                onClick={() => handleFilterChange("TE")}>TE</div>
+                            <div className={`players-page-filter-button${activeFilter === "K" ? "-active" : ""}`}
+                                onClick={() => handleFilterChange("K")}>K</div>
+                            <input
+                                className="players-page-search-bar"
+                                type="text"
+                                placeholder="Search"
+                                onChange={event => setSearchInput(event.target.value)} />
                         </div>
                     </div>
                     <div className="players-page-player-info-headers">
@@ -92,7 +126,11 @@ const PlayersPage = () => {
                         {/* TODO: Add composite 2024 fantsy points */}
                     </div>
                     <div className="players-page-player-object-container">
-                        <PlayerObjectList players={resPlayers} />
+                        <PlayerObjectList players={resPlayers} filter={activeFilter} />
+                        {showError && (<div className="players-page-error-message-container">
+                            <div className="players-page-error-message-box">
+                                <FiAlertTriangle size={64} />{error}, Please try again later.</div>
+                            </div>)}
                     </div>
                 </div>
             </div>
